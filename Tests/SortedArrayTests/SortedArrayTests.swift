e801e0fe992816b4c16221dcdee83dfcc03c4eca
Adam Nemecek <adamnemecek@gmail.com>
2017-05-04 08:41:02 -0700

    initial

---
+++ Tests/SortedArrayTests/SortedArrayTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import SortedArray
+
+class SortedArrayTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(SortedArray().text, "Hello, World!")
+    }
+
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
